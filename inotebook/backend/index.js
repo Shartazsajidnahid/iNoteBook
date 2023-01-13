32080895458094d0fdd4b83b467a93ef55a385bc
Nahid <sajidnahid9658@gmail.com>
2023-01-13 15:39:51 +0600

    models and routes init

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`iNoteBook backend listening on port ${port}`)
 })
