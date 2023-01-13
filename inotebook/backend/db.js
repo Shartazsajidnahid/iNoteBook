7f82d2b6122642ee2543c9cffb39e972dfd183a1
Nahid <sajidnahid9658@gmail.com>
2023-01-13 14:12:00 +0600

    models and routes: init

--- inotebook/backend/db.js
+++ inotebook/backend/db.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 
 const mongoURI = "mongodb://localhost:27017"
 
+mongoose.set("strictQuery", false);
+
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
         console.log("Connected to Mongo Successfullyy");
