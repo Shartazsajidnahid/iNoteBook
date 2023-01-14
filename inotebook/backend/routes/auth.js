dbff91f756037e79df30386a69d0cdfa2f02fff1
Nahid <sajidnahid9658@gmail.com>
2023-01-14 23:08:28 +0600

    s

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,9 +1,48 @@
-const express = require("express");
+
+const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
+
 
 router.get("/", async (req, res) => {
   console.log(req.body);
   res.send("from auth.js");
 });
+
+// ROUTE 1: Create a User using: POST "/api/auth/createuser". No login required
+router.post('/createuser', [
+  body('name', 'Enter a valid name').isLength({ min: 3 }),
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password must be atleast 3 characters').isLength({ min: 3 }),
+], async (req, res) => {
+  // If there are errors, return Bad request and the errors
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  try {
+    // Check whether the user with this email exists already
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ error: "Sorry a user with this email already exists" })
+    }
+    // Create a new user
+    user = await User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+    });
+
+
+
+    // res.json(user)
+    res.json("success");
+
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+})
 
 module.exports = router;
