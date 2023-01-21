8b1176ae2121188adf5d1d33ce8cd7d17a0bb28a
Shartazsajidnahid <sajidnahid9658@gmail.com>
2023-01-21 17:31:59 +0600

    components: home, navbar, about initiated. context with state used.

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -1,8 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import About from './components/About'; 
+import NoteState from './context/notes/NoteState';
 
 function App() {
-  return <div className="App"> iNoteBook</div>;
+  return (
+    <> 
+    <NoteState>
+      <Router>
+        <Navbar /> 
+        <Routes>
+          <Route exact path="/" element={<Home/>}> </Route>
+          <Route exact path="/about" element={<About/>}> </Route> 
+        </Routes>
+      </Router> 
+      </NoteState>
+    </>
+  );
 }
 
 export default App;
