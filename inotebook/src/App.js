3c95c1d5fa73b46ee1056ab00266ab68cc98219a
Shartazsajidnahid <sajidnahid9658@gmail.com>
2023-01-22 20:33:31 +0600

    notes and noteitems in frontend

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import Navbar from './components/Navbar';
 import { Home } from './components/Home';
-import About from './components/About'; 
+import About from './components/About';
 import NoteState from './context/notes/NoteState';
 
 function App() {
@@ -12,10 +15,12 @@ function App() {
     <NoteState>
       <Router>
         <Navbar /> 
-        <Routes>
-          <Route exact path="/" element={<Home/>}> </Route>
-          <Route exact path="/about" element={<About/>}> </Route> 
-        </Routes>
+          <div className='container'>
+            <Routes>
+              <Route exact path="/" element={<Home/>}> </Route>
+              <Route exact path="/about" element={<About/>}> </Route> 
+            </Routes>
+          </div>
       </Router> 
       </NoteState>
     </>
