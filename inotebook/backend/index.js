0561f79a16d93b4824e7424bc008d97c3c83c9da
Nahid <sajidnahid9658@gmail.com>
2023-01-25 02:49:06 +0600

    add note, delete, only frontend. fetch notes from backend.

--- inotebook/backend/index.js
+++ inotebook/backend/index.js
@@ -1,20 +1,19 @@
+const connectToMongo = require('./db');
 const express = require('express')
-const connectToMongo = require('./db')
+var cors = require('cors') 
+
+connectToMongo();
 const app = express()
 const port = 5000
 
-app.use(express.json());
-connectToMongo();
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
+app.use(cors())
+app.use(express.json())
 
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
 
+
 app.listen(port, () => {
-    console.log(`iNoteBook backend listening on port ${port}`)
+  console.log(`iNotebook backend listening at http://localhost:${port}`)
 })
