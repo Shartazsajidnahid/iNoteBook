9f9fe500ae511bc9028d1506b27cc647573e92a6
Shartazsajidnahid <sajidnahid9658@gmail.com>
2023-01-28 15:51:02 +0600

    signup done, auth-token in response

--- inotebook/src/components/Login.js
+++ inotebook/src/components/Login.js
@@ -8,6 +8,7 @@ const Login = (props) => {
     const [credentials, setCredentials] = useState({email: "", password: ""}) 
     
     const navigate = useNavigate();
+    
     const handleSubmit = async (e) => {
         e.preventDefault();
         const response = await fetch("http://localhost:5000/api/auth/login", {
